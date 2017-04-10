578605a5aa4ecbdab83fc8da22cd746d777f223e
Silvan Heller <silvan.heller@unibas.ch>
2017-04-10 17:42:58 +0200

    Fixing copy-paste typo in OperationType

--- src/main/scala/ch/unibas/dmi/hs17/dis/ops/OperationType.scala
+++ src/main/scala/ch/unibas/dmi/hs17/dis/ops/OperationType.scala
@@ -1,9 +1,9 @@
-package ch.unibas.dmi.hs17.dis.main
+package ch.unibas.dmi.hs17.dis.ops
 
 /**
   * Created by silvan on 10.04.17.
   */
 object OperationType extends Enumeration{
-  type StorageMode = Value
+  type OperationType = Value
   val Write, Read, Query = Value
 }
